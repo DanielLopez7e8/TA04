10f48a95a8e707af76c99f0ffb90735b0215ba7a
DanielLopez7e8 <daniel.lopez.7e8@itb.cat>
2024-12-01 20:42:37 +0100

    Final version

--- js/alerta.js
+++ js/alerta.js
@@ -4,5 +4,5 @@ const telefonoTexto = document.getElementById('alerta');
 // Agregar un evento de clic al elemento
 telefonoTexto.addEventListener('click', () => {
     // Mostrar la alerta con el número de teléfono
-    alert('El número telefónico de Bryan es: +34 123 456 890');
+    alert('El número telefónico de Dani es: +34 691 974 355');
 });
